4b6c756dae04d7ec48ac871cfa71a9f107ed56cb
Антон Дмитриевич Пастухов <pastuhov@alma.mater>
2021-11-30 12:54:51 +0300

    initialized

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * by anton pastukhov
+ * andm1793@gmail.com
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  return(
+    <Text>JOURNAL</Text>
+  );
+};
+
+export default App;
